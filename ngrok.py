38b2bcec5d02e0f3ff55bfe661ba394287271bcb
Megah <75638685+endblack@users.noreply.github.com>
2024-08-06 00:49:07 -0300

    Update ngrok.py

--- ngrok.py
+++ ngrok.py
@@ -1,4 +1,6 @@
 import subprocess
+import time
+import requests
 
 # Configura seu token de autenticação do ngrok
 #NGROK_AUTH_TOKEN = "your_ngrok_auth_token_here"
@@ -10,6 +12,25 @@ import subprocess
 ngrok_process = subprocess.Popen(["./ngrok/ngrok.exe", "tcp", "3389"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 print("Ngrok OK")
+def get_ngrok_public_url():
+    try:
+        response = requests.get("http://localhost:4040/api/tunnels")
+        response.raise_for_status()
+        tunnels = response.json().get("tunnels", [])
+        if tunnels:
+            for tunnel in tunnels:
+                public_url = tunnel['public_url']
+                print(f"Ngrok Url: {public_url}")
+                return public_url
+        else:
+            print("Nenhum túnel ngrok ativo encontrado.")
+            return None
+    except requests.RequestException as e:
+        print(f"Erro ao verificar o status do ngrok: {e}")
+        return None
+
+public_url = get_ngrok_public_url()
+
 #print("ngrok iniciado e em execução em segundo plano.")
 #print("Você pode verificar os túneis em http://localhost:4040/status")
 
